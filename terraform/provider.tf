6504eec4799b9cdfebcc09e74773319f79ba8b85
aiman10 <37143774+aiman10@users.noreply.github.com>
2026-05-26 10:21:35 +0200

    s3 bucket and terraform backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,3 +8,11 @@ terraform {
 }
 
 provider "aws" {}
+
+terraform {
+    backend "s3" {
+        bucket = "tf-remote-backend-ehb-2660"
+        key    = "path/to/my/key"
+        region = "eu-west-1"
+    }
+}
--- terraform/s3.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "this" {
-  bucket = "mlops-course-dev-863745572000"
-
-  tags = {
-    Environment = "Dev"
-  }
-}
